e011651e01ad9dd839b84d7782ceee36686f16ba
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-09-24 20:10:39 +0100

    testing inline and external styling

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+
+Table of Contents
+
+| Topic | Link | Started |
+| --- | --- | --- |
+| Portfolio | [title](github.com/AffirmedVisionary/blackcodher-bootcamp/tree/master/html/portfolio) | In Unit 1
+| Bookshop | [title](https://github.com/AffirmedVisionary/blackcodher-bootcamp/tree/master/html/bookshop) | in Unit 2
+| Quiz | [title](https://github.com/AffirmedVisionary/blackcodher-bootcamp/tree/master/javascript/quiz-along) | in Unit 3
+| React myfirstapp | [title]
--- react-and-react-native/myfirstapp/src/App.js
+++ react-and-react-native/myfirstapp/src/App.js
@@ -2,22 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  // const h1Style = {"fontSize":"30px", "color":"#000000"};
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 
+        // style={h1Style} 
+        className = "testHeader">
+          Learning React by creating our own mini application
+        </h1>
       </header>
     </div>
   );
